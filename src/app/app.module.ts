5bfb3bf857820e39521ebab029eb8cb40f9bc865
GrungeElFz <elf0114_@hotmail.com>
2023-07-10 16:45:24 -0500

    Initialized:    `ng generate component password-list`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SiteListComponent } from './site-list/site-list.component';
+import { PasswordListComponent } from './password-list/password-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SiteListComponent
+    SiteListComponent,
+    PasswordListComponent
   ],
   imports: [
     BrowserModule,
